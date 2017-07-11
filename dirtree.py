eb888216271b01078fed5bf5078fbbe6542529a5
Quy Le <lnquy.it@gmail.com>
2017-07-11 13:15:23 +0700

    Pretty print file size

--- dirtree.py
+++ dirtree.py
@@ -1,7 +1,7 @@
 # Exercise project when learning Python =))
-# DirTree recursively look for all child folders/files in a directory and draw its directory tree to the console
+# DirTree recursively look for all child folders/files in a directory path and draw its directory tree to the console
 # --------------------------
-# @author: lnquy
+# @author: lnquy (Quy Le)
 # @email: lnquy.it@gmail.com
 # @date: Jul 11 2017
 # --------------------------
@@ -16,6 +16,7 @@ CYAN = '\033[0;36m'
 NC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
+SIZE = ("B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
 
 
 def green(s):
@@ -30,16 +31,24 @@ def blue(s):
     print(BLUE + s + NC)
 
 
+def pretty_size(fpath):
+    s = path.getsize(fpath)
+    i = 1
+    while s // 1000 > 0:
+        s /= 1000
+        i += 1
+    return ("%.1f" % s) + SIZE[i - 1]
+
+
 def watcher(dpath, level):
     try:
-        level += 1
         for f in listdir(dpath):
             abs_path = path.join(dpath, f)
             if path.isdir(abs_path):
-                blue(("  |" * (level - 1)) + "  +--" + f)
-                watcher(abs_path, level)
+                print(("  |" * level) + "  +--" + BLUE + f + NC)
+                watcher(abs_path, level + 1)
             else:
-                print(("  |" * (level - 1)) + "  |--" + f + " - " + str(path.getsize(abs_path)))
+                print(("  |" * level) + "  |--" + f + " - " + pretty_size(abs_path))
     except IOError:  # Permission denied
         pass
 
@@ -48,6 +57,5 @@ print("Welcome to DirTree!")
 dir_path = input("Please enter the directory path: ")
 if not dir_path:
     dir_path = os.getcwd()
-print("Dir: ", dir_path)
+print(dir_path)
 watcher(dir_path, 0)
-
