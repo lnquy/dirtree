f6fa03b689ffecd197a73d9fe6dc17f040021586
Quy Le <lnquy.it@gmail.com>
2017-07-12 17:00:34 +0700

    Folder, file counters. Run as shell script

--- dirtree.py
+++ dirtree.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # Exercise project when learning Python =))
 # DirTree recursively look for all child folders/files in a directory path and draw its directory tree to the console
 # --------------------------
@@ -7,11 +8,14 @@
 # --------------------------
 
 import os
+import sys
 from os import listdir, path
 
 BLUE = '\033[0;34m'
 NC = '\033[0m'
 SIZE = ("B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+folders_num = 0
+files_num = 0
 
 
 def pretty_size(fpath):
@@ -24,22 +28,26 @@ def pretty_size(fpath):
 
 
 def watcher(dpath, level):
+    global folders_num, files_num
     try:
         for f in listdir(dpath):
             abs_path = path.join(dpath, f)
             if path.isdir(abs_path):
+                folders_num += 1
                 print(("  |" * level) + "  +--" + BLUE + f + NC)
                 watcher(abs_path, level + 1)
             else:
+                files_num += 1
                 print(("  |" * level) + "  |--" + f + " - " + pretty_size(abs_path))
     except IOError:  # Permission denied
         pass
 
 
 # Start
-print("Welcome to DirTree!")
-dir_path = input("Please enter the directory path: ")
-if not dir_path:
-    dir_path = os.getcwd()
+dir_path = os.environ["PWD"]
+if len(sys.argv) >= 2:
+    dir_path = sys.argv[1]
 print(dir_path)
 watcher(dir_path, 0)
+print("\n" + str(folders_num) + (" folder" if folders_num <= 1 else " folders") + ", " +
+      str(files_num) + (" file" if files_num <= 1 else " files"))
